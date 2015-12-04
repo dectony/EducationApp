f57088f6502c5c2fb5f65c27d4629109f302126f
unknown <anton.zasenka@wsa-147.icx.local>
2015-12-04 15:25:52 +0300

    first commit

---
+++ EducationApp.DataAccess/Entities/Quiz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationApp.DataAccess.Entities
+{
+    public class Quiz
+    {
+        public int QuizId { get; set; }
+        public string QuizName { get; set; }
+
+        public virtual List<QuizQuestion> Questions { get; set; }
+        public virtual Theme Theme { get; set; }
+    }
+}
---
+++ EducationApp.DataAccess/Entities/QuizAnswer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationApp.DataAccess.Entities
+{
+    public class QuizAnswer
+    {
+        public int QuizAnswerId { get; set; }
+        public string Answer { get; set; }
+
+        public virtual QuizQuestion Question { get; set; }
+    }
+}
